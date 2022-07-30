81bf72d72a338847d37493c5935598912197d1ef
qal1215 <75682994+qal1215@users.noreply.github.com>
2022-07-30 19:20:20 +0700

    set env

--- server/index.js
+++ server/index.js
@@ -1,39 +1,39 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const authRouter = require('./routes/auth')
-const postRouter = require('./routes/post')
+const authRouter = require("./routes/auth");
+const postRouter = require("./routes/post");
 
 const connectDB = async () => {
-	try {
-		await mongoose.connect(
-			`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-learnit.j0fcc.mongodb.net/mern-learnit?retryWrites=true&w=majority`,
-			{
-				useCreateIndex: true,
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-				useFindAndModify: false
-			}
-		)
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@learnit.wyhg9.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      }
+    );
 
-		console.log('MongoDB connected')
-	} catch (error) {
-		console.log(error.message)
-		process.exit(1)
-	}
-}
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
-connectDB()
+connectDB();
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.use('/api/auth', authRouter)
-app.use('/api/posts', postRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
